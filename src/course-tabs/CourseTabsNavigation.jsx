a51bf6319cf8dc60a00eab03dcb4016d0b189d0b
mahtisham1 <110836471+mahtisham1@users.noreply.github.com>
2023-05-22 21:00:04 +0500

    Update CourseTabsNavigation.jsx

--- src/course-tabs/CourseTabsNavigation.jsx
+++ src/course-tabs/CourseTabsNavigation.jsx
@@ -11,20 +11,21 @@ const CourseTabsNavigation = ({
 }) => (
   <div id="courseTabsNavigation" className={classNames('course-tabs-navigation', className)}>
     <div className="container-xl">
-      <Tabs
-        className="nav-underline-tabs"
-        aria-label={intl.formatMessage(messages.courseMaterial)}
-      >
-        {tabs.map(({ url, title, slug }) => (
-          <a
-            key={slug}
-            className={classNames('nav-item flex-shrink-0 nav-link', { active: slug === activeTabSlug })}
-            href={url}
-          >
-            {title}
-          </a>
-        ))}
-      </Tabs>
+      <nav className="nav flex-column nav-pills">
+        <div aria-label={intl.formatMessage(messages.courseMaterial)}>
+        <div className="nav-items-container">
+            {tabs.map(({ url, title, slug }) => (
+              <a
+                key={slug}
+                className={classNames('nav-link', { active: slug === activeTabSlug })}
+                href={url}
+              >
+                {title}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
     </div>
   </div>
 );
